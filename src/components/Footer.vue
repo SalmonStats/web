dca7d84af63bb44526982cf1178f4c8ab050a940
tkgstrator <nasawake.am@gmail.com>
2022-05-29 03:37:46 +0900

    - Template views

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,24 +10,24 @@ const toggleLocales = () => {
 
 <template>
   <nav text-xl mt-6>
-    <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
+    <RouterLink class="icon-btn mx-2" to="/">
       <div i-carbon-campsite />
     </RouterLink>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+    <button class="icon-btn mx-2 !outline-none" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
+    <a class="icon-btn mx-2" @click="toggleLocales()">
       <div i-carbon-language />
     </a>
 
-    <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+    <RouterLink class="icon-btn mx-2" to="/about">
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://twitter.com/tkgling" target="_blank" title="GitHub">
+      <div i-carbon-logo-twitter />
     </a>
   </nav>
 </template>
---
+++ src/components/Ranking.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from '@vue/reactivity'
+const { t, availableLocales, locale } = useI18n()
+
+interface Result {
+  salmon_id: number
+  golden_ikura_num: number
+  ikura_num: number
+}
+
+interface Results {
+  limit: number
+  offset: number
+  results: Result[]
+}
+
+const results: Results = ref<Results>(await (await fetch('https://api.splatnet2.com/v1/results?order=false')).json())
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>{{ t("title.salmon_id") }}</th>
+        <th>{{ t("title.golden_ikura_num") }}</th>
+        <th>{{ t("title.ikura_num") }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="result in results.results" :key="result.salmon_id">
+        <td>{{ result.salmon_id }}</td>
+        <td>{{ result.golden_ikura_num }}</td>
+        <td>{{ result.ikura_num }}</td>
+      </tr>
+    </tbody>
+    <!-- <li v-for="result in results.results" :key="result.salmon_id">
+      {{ result.salmon_id }}
+    </li> -->
+  </table>
+</template>
