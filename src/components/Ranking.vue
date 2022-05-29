db4ae4954ccec26533bf59e6c07cbd0f2b931653
tkgstrator <nasawake.am@gmail.com>
2022-05-29 17:38:18 +0900

    - Add assets from SplatNet2

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -6,6 +6,7 @@ interface Result {
   salmon_id: number
   golden_ikura_num: number
   ikura_num: number
+
 }
 
 interface Results {
@@ -18,23 +19,94 @@ const results: Results = ref<Results>(await (await fetch('https://api-dev.splatn
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>{{ t("title.salmon_id") }}</th>
-        <th>{{ t("title.golden_ikura_num") }}</th>
-        <th>{{ t("title.ikura_num") }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="result in results.results" :key="result.salmon_id">
-        <td>{{ result.salmon_id }}</td>
-        <td>{{ result.golden_ikura_num }}</td>
-        <td>{{ result.ikura_num }}</td>
-      </tr>
-    </tbody>
-    <!-- <li v-for="result in results.results" :key="result.salmon_id">
-      {{ result.salmon_id }}
-    </li> -->
-  </table>
+  <div class="stats-detail">
+    <div class="personal-stats-wrapper" />
+    <div class="coop-stats">
+      <ul class="coop-stats-list">
+        <li>
+          <a class=" internal-link" href="/coop/results/2296">
+            <ul class="coop-stats-list-content clear">
+              <li class="job-result">Clear!</li>
+              <li class="grade">Profreshional <span class="grade-point">620</span><span class="up" /></li>
+              <li class="ikura-result">
+                <p class="golden-ikura">
+                  <span class="screen-reader-text">Golden Eggs collected</span>
+                  <span class="num">44</span>
+                </p>
+                <p class="ikura">
+                  <span class="screen-reader-text" />
+                  <span class="num">734</span>
+                </p>
+              </li>
+            </ul>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.coop-stats-list-content {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    background: rgba(0,0,0,.75);
+    margin-bottom: 8px;
+    height: 48px;
+    border-radius: 24px;
+}
+
+.num::before {
+  content: "X";
+  font-size: 11px;
+  padding-right: 2px;
+}
+
+.golden-ikura, .ikura {
+  position: relative;
+  line-height: 20px;
+  padding: 0 12px 0 26px;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+
+.golden-ikura:before {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+  content: "";
+  background-repeat: no-repeat;
+  background-position: 50%;
+  height: 20px;
+  width: 24px;
+  background-image: url(/images/bundled/3aa6fb4ec1534196ede450667c1183dc.png);
+  background-size: 18px 18px;
+}
+.ikura:before {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+  content: "";
+  background-repeat: no-repeat;
+  background-position: 50%;
+  height: 20px;
+  width: 24px;
+  background-image: url(/images/bundled/efe826cfd1d44d19153f08e19f6caa2a.png);
+  background-size: 20.5px 15px;
+}
+
+.screen-reader-text {
+    position: absolute!important;
+    clip: rect(1,1,1,1);
+    display: none;
+}
+</style>
