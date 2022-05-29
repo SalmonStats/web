99997d07c4ce57d97df9816eeaec1f132ff0ceb0
tkgstrator <nasawake.am@gmail.com>
2022-05-29 18:19:21 +0900

    - 見た目をそれっぽく変更

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -2,11 +2,15 @@
 import { ref } from '@vue/reactivity'
 const { t, availableLocales, locale } = useI18n()
 
+interface JobResult {
+  is_clear: boolean
+}
+
 interface Result {
   salmon_id: number
   golden_ikura_num: number
   ikura_num: number
-
+  job_result: JobResult
 }
 
 interface Results {
@@ -23,23 +27,27 @@ const results: Results = ref<Results>(await (await fetch('https://api-dev.splatn
     <div class="personal-stats-wrapper" />
     <div class="coop-stats">
       <ul class="coop-stats-list">
-        <li>
-          <a class=" internal-link" href="/coop/results/2296">
-            <ul class="coop-stats-list-content clear">
-              <li class="job-result">Clear!</li>
-              <li class="grade">Profreshional <span class="grade-point">620</span><span class="up" /></li>
+        <li v-for="result in results.results" :key="result.salmon_id">
+          <router-link class=" internal-link" :to="`/results/${result.salmon_id}`">
+            <ul class="coop-stats-list-content" :class="{ clear: result.job_result.is_clear, failure: !result.job_result.is_clear }">
+              <li class="job-result">
+                {{ result.job_result.is_clear ? t("job_result.is_clear") : t("job_result.is_failure") }}
+              </li>
+              <li class="grade">
+                Profreshional <span class="grade-point">620</span><span class="up" />
+              </li>
               <li class="ikura-result">
                 <p class="golden-ikura">
                   <span class="screen-reader-text">Golden Eggs collected</span>
-                  <span class="num">44</span>
+                  <span class="num">{{ result.golden_ikura_num }}</span>
                 </p>
                 <p class="ikura">
                   <span class="screen-reader-text" />
-                  <span class="num">734</span>
+                  <span class="num">{{ result.ikura_num }}</span>
                 </p>
               </li>
             </ul>
-          </a>
+          </router-link>
         </li>
       </ul>
     </div>
@@ -64,15 +72,32 @@ const results: Results = ref<Results>(await (await fetch('https://api-dev.splatn
   padding-right: 2px;
 }
 
-.golden-ikura, .ikura {
+.golden-ikura {
   position: relative;
   line-height: 20px;
   padding: 0 12px 0 26px;
+  text-align: left;
+}
+
+.ikura {
+  position: relative;
+  line-height: 20px;
+  padding: 0 12px 0 26px;
+  text-align: left;
 }
 
 p {
   margin: 0;
   padding: 0;
+}
+
+a {
+  cursor: pointer;
+}
+
+.ikura-result {
+  flex-grow: 0;
+  width: 80px;
 }
 
 .golden-ikura:before {
@@ -89,6 +114,7 @@ p {
   background-image: url(/images/bundled/3aa6fb4ec1534196ede450667c1183dc.png);
   background-size: 18px 18px;
 }
+
 .ikura:before {
   position: absolute;
   top: 50%;
@@ -108,5 +134,51 @@ p {
     position: absolute!important;
     clip: rect(1,1,1,1);
     display: none;
+}
+
+.coop-stats-list {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 0 10px;
+    line-height: 1;
+    box-sizing: border-box;
+}
+
+.coop-stats-list-content.clear .job-result {
+  color: #39e464;
+  font-family: Splatoon1;
+  width: 90px;
+  font-size: 14px;
+}
+
+.coop-stats-list-content.failure .job-result {
+  color: #ff7500;
+  font-family: Splatoon1;
+  width: 90px;
+  font-size: 14px;
+}
+
+.coop-stats-list-content.clear .grade {
+  color: #fff;
+  font-family: Splatoon1;
+  flex-grow: 1;
+}
+
+.coop-stats-list-content.failure .grade {
+  color: #777;
+  font-family: Splatoon1;
+  flex-grow: 1;
+}
+
+.coop-stats-list-content .up {
+    display: inline-block;
+    background-image: url(/images/bundled/ae680e6….png);
+    margin-left: 8px;
+    width: 24px;
+    height: 24px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    vertical-align: middle;
 }
 </style>
