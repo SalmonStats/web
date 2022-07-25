7103e3802e19ec4e747a7cdb814aff7901382865
tkgstrator <nasawake.am@gmail.com>
2022-07-25 21:52:45 +0900

    - 見た目を修正

--- src/components/CoopResults.vue
+++ src/components/CoopResults.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { getCurrentInstance, onMounted, Ref, ref } from 'vue';
-import { IonList, IonItem, IonLabel, IonContent } from '@ionic/vue';
+import { IonList, IonItem, IonLabel, IonContent, IonRow, IonCol } from '@ionic/vue';
 import { useI18n } from 'vue-i18n'
 import { Result } from './@types/response';
+import dayjs from 'dayjs';
 
 const results: Ref<Result[]> = ref([]);
 const app = getCurrentInstance()
+const { t } = useI18n()
 
 onMounted(() => {
   const baseURL = app?.appContext.config.globalProperties.$baseURL
@@ -25,24 +27,44 @@ onMounted(() => {
 <template>
   <ion-content>
     <ion-list>
-      <template v-for="result in results" :key="result.salmon_id">
-        <ion-item button :router-link="`/results/${result.salmon_id}`">
-          <ion-label>
-            <p>{{ result.salmon_id }}</p>
-            <p>{{ result.job_result.is_clear }}</p>
-            <p>{{ result.danger_rate }}</p>
-          </ion-label>
-          <ion-label>
-            <ion-label v-for=" wave in result.waves" :key="wave.quota_num">
-              <p>{{ wave.water_level }} {{ wave.event_type }}</p>
-            </ion-label>
-          </ion-label>
-          <ion-label>
-            <p>{{ result.golden_ikura_num }}</p>
-            <p>{{ result.ikura_num }}</p>
-          </ion-label>
-        </ion-item>
-      </template>
+      <ion-item-group>
+        <template v-for="result in results" :key="result.salmon_id">
+          <ion-item button :router-link="`/results/${result.salmon_id}`">
+            <ion-grid>
+              <ion-row>
+                <ion-col size="2">
+                  <ion-row>
+                    {{ result.salmon_id }}
+                  </ion-row>
+                  <ion-row>
+                    {{ result.job_result.is_clear }}
+                  </ion-row>
+                  <ion-row>
+                    {{ result.danger_rate }}
+                  </ion-row>
+                </ion-col>
+                <ion-col>
+                  <template v-for="wave in result.waves" :key="wave.quota_num">
+                    <ion-row>
+                      {{ wave.golden_ikura_num }}/{{ wave.quota_num }}({{ wave.golden_ikura_pop_num }}) {{
+                          t(`event_type.${wave.event_type}`)
+                      }}
+                    </ion-row>
+                  </template>
+                </ion-col>
+                <ion-col size="auto">
+                  <ion-row>
+                    {{ result.golden_ikura_num }}
+                  </ion-row>
+                  <ion-row>
+                    {{ result.ikura_num }}
+                  </ion-row>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-item>
+        </template>
+      </ion-item-group>
     </ion-list>
   </ion-content>
 </template>
