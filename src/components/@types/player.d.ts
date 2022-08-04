41064b01667c4f8cd47faa0e85de61714d4532ca
tkgstrator <nasawake.am@gmail.com>
2022-08-05 07:44:55 +0900

    - ちょっと修正

--- src/components/@types/player.d.ts
+++ src/components/@types/player.d.ts
@@ -1,38 +1,70 @@
-export interface Player {
-  nsaid: string;
-  nickname: string;
-  thumbnail_url: string;
-  shifts_worked: number;
-  is_clear: number;
-  is_failure: number;
-  is_verified: boolean;
-  is_friend_code_public: boolean;
-  is_twitter_id_public: boolean;
-  is_imperial_scholars: boolean;
-  golden_ikura_num: number;
-  ikura_num: number;
-  kuma_point: number;
-  grade_point: number;
-  stage_results: StageResult[];
-}
+export namespace SalmonStats {
+  /**
+   * Salmon Statsに登録されているプレイヤー情報
+   */
+  export interface Player {
+    /** プレイヤーID */
+    nsaid: string;
+    /** プレイヤー名 */
+    nickname: string;
+    /** サムネイルURL */
+    thumbnail_url: string;
+    /** バイト回数 */
+    shifts_worked: number;
+    /** クリア回数 */
+    is_clear: number;
+    /** 失敗回数 */
+    is_failure: number;
+    /** 認証済みかどうか */
+    is_verified: boolean;
+    /** フレンドコードが公開設定かどうか */
+    is_friend_code_public: boolean;
+    /** TwitterIDが公開設定かどうか */
+    is_twitter_id_public: boolean;
+    /** 特待生どうか */
+    is_imperial_scholars: boolean;
+    /** 総合金イクラ納品数 */
+    golden_ikura_num: number;
+    /** 総合イクラ獲得数 */
+    ikura_num: number;
+    /** 総合クマサンポイント獲得数 */
+    kuma_point: number;
+    /** 最高評価 */
+    grade_point: number;
+    /** ステージごとの最高納品数記録 */
+    stage_results: Results[];
+  }
 
-interface StageResult {
-  stage_id: number;
-  night: Result;
-  nightless: Result;
-}
+  export interface Results {
+    /** ステージID */
+    stage_id: number;
+    /** 夜記録 */
+    night: Result;
+    /** 昼記録 */
+    nightless: Result;
+  }
 
-interface Result {
-  stage_id: number;
-  nightless: boolean;
-  grade_point_max: number;
-  shifts_worked: number;
-  kuma_point_total: number;
-  player_results: EggsResult;
-  team_results: EggsResult;
-}
+  interface Result {
+    /** ステージID */
+    stage_id: number;
+    /** 夜イベントを含むかどうか */
+    nightless: boolean;
+    /** 最高評価 */
+    grade_point_max: number;
+    /** バイト回数 */
+    shifts_worked: number;
+    /** 合計クマサンポイント獲得数 */
+    kuma_point_total: number;
+    /** 個人記録 */
+    player_results: Record;
+    /** チーム記録 */
+    team_results: Record;
+  }
 
-interface EggsResult {
-  golden_ikura_num: number;
-  ikura_num: number;
+  interface Record {
+    /** 金イクラ納品数 */
+    golden_ikura_num: number;
+    /** イクラ獲得数 */
+    ikura_num: number;
+  }
 }
